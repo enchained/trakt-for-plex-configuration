5d32e38372f739195149eda0eb2ae6653de97162
Anna Gerasimova <enchained@users.noreply.github.com>
2026-07-30 04:14:07 +0300

    Remove debug line

--- app/scripts/models/plex/connection_manager.js
+++ app/scripts/models/plex/connection_manager.js
@@ -75,14 +75,6 @@ angular.module('configurationApp')
       // Test connection
       var deferred = $q.defer();
 
-      console.log(
-        '[Plex debug] token present:',
-        typeof self.server.token_plex === 'string' &&
-          self.server.token_plex.length > 0,
-        'length:',
-        self.server.token_plex ? self.server.token_plex.length : 0
-      );
-
       client.identity({
         timeout: 10000
       }).then(function(response) {
